615e97de4a5e5737ba7ebdf15e2a28cf7b03f5e7
Myles Cashwell <mcashwell97@gmail.com>
2020-11-15 23:06:48 -0800

    Added PersistenceManager

--- FirstProject/Utilities/FPError.swift
+++ FirstProject/Utilities/FPError.swift
@@ -13,4 +13,6 @@ enum FPError: String, Error {
     case unableToComplete = "Unable to complete request. Please check internet connection."
     case invalidResponse  = "Invalid response from the server. Please try again."
     case invalidData      = "The data recieved from the server is invalid. Please try again."
+    case unableToFavorite = "Unable to Favorite user. Please try another user."
+    case alreadyFavorited = "This user is already in your list of favorite users."
 }
